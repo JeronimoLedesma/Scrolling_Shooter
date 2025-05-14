f21f6c80ebadece1b13e609c2221cd7a2b344bab
JeronimoLedesma <ledesmajero@gmail.com>
2025-05-14 13:09:32 -0300

    Player Function

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Move")]
+    Vector2 moveInput;
+    [SerializeField] float speed;
+    [SerializeField] float limitx;
+    [SerializeField] float limity;
+
+    [Header("Shoot")]
+    [SerializeField] GameObject bulletSpawner;
+    [SerializeField] GameObject bullet;
+
+    [Header("Life")]
+    [SerializeField] float life;
+    bool beingHit;
+    [SerializeField] float stun;
+    [SerializeField] Slider healthBar;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void OnMove (InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnShoot (InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //Logica de disparo
+        }
+    }
+    
+    private void Move()
+    {
+        Vector3 pos = transform.position;
+        pos += new Vector3(moveInput.x * speed * Time.deltaTime, moveInput.y * speed * Time.deltaTime, 0f);
+        pos.x = Mathf.Clamp(pos.x, -limitx, limitx);
+        pos.y = Mathf.Clamp(pos.y, -limity, limity);
+        transform.position = pos;
+    }
+}
